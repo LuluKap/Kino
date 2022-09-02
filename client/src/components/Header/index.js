e3b96b0c22d5b5fe5a9364780ae93354a64ccbf4
Nridha0 <ridha.noor@yahoo.com>
2022-09-02 09:17:08 -0500

    change in header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,9 +13,9 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">Kino</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">your movies blog</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
