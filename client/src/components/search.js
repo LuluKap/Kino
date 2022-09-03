3821b878afb528d2b41868579fbd5d622e6a9ffa
Lulu Kapita <lulukapita@gmail.com>
2022-09-03 02:30:05 -0400

    api official connection

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,35 +1,64 @@
-import React from "react";
-// import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Searchbar() {
-//   const { 
-//     onSearch 
-//   } = props;
 
-//   const [searchText, setSearchText] = useState('')
+  const [endPoint, setEndPoints] = useState('')
 
-//   const handleInput = (e) => {
-//     const text = e.target.value
-//     setSearchText(text)
-//   }
+  const [container, setContainer] = useState([])
 
-//   const handleEnterKeyPressed = (e) => {
-//     if(e.key=== 'Enter') {
-//       onSearch(searchText)
-//     }
-//   }
+  const [finalPoint, setFinalPoint] = useState('');
+    
+useEffect(() =>{
+  fetchMe()
+},[finalPoint])
+
+  const fetchMe = () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'fb165f2f83msh3819e46f1f866c9p199f40jsne52d7734edf2',
+      'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com'
+    }
+  };
+
+  fetch(`https://unogs-unogs-v1.p.rapidapi.com/search/titles?order_by=title_asc&title=+${endPoint}`, options)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      setContainer(data.results)
+    })
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  }
+
+const onChangeHandler = (e) => {
+  setEndPoints(e.target.value)
+}
+const submitHandler = e => {
+  e.preventDefault()
+  setFinalPoint(endPoint)
+}
 
   return (
     <div>
       <div className="control"> 
+      <form onSubmit={submitHandler}>
         <input
           className="input"
-        //   onChange={handleInput}
-        //   onKeyPress={handleEnterKeyPressed}
-          type="text"
-        //   value={searchText}
-          placeholder="Search your movies"
-        />
+          type="text" value={endPoint} onChange={onChangeHandler} placeholder="Search your movies"/>
+          <button type='submit'>submit</button>
+          </form>
+{/* <div className="element">
+          {container.map((item,index) => {
+            return(
+              <div key={index} className='element-div'> 
+              <img src={item.img} alt="" />
+              <p>{item.title}</p>
+              </div>
+            )
+          })}
+          </div> */}
       </div>
     </div>
   );
